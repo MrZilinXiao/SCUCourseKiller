7a4d7977942cdb42c28ec9de53dad11150459b47
MrZilinXiao <me@mrxiao.net>
2019-12-30 16:54:41 +0800

    Update apps.py

--- SCUKiller/apps.py
+++ SCUKiller/apps.py
@@ -8,8 +8,9 @@ from SCUKiller.config import watch_interval
 def watchLoop():
     pass
     from SCUKiller import utils
+    lock = threading.Lock()
     while True:  # watch将导致数据库不同步 很烦
-        t = threading.Thread(target=utils.watchCourses)
+        t = threading.Thread(target=utils.watchCourses, args=(None, lock))
         t.start()
         time.sleep(random.random() + watch_interval)
 
