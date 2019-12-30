b1d59cbaa168786a1a191200607118e1d351d92e
MrZilinXiao <me@mrxiao.net>
2019-12-30 12:24:11 +0800

    enable thread

--- SCUKiller/apps.py
+++ SCUKiller/apps.py
@@ -7,11 +7,11 @@ from SCUKiller.config import watch_interval
 
 def watchLoop():
     pass
-    # from SCUKiller import utils
-    # while True:  # watch将导致数据库不同步 很烦
-    #     t = threading.Thread(target=utils.watchCourses)
-    #     t.start()
-    #     time.sleep(random.random() + watch_interval)
+    from SCUKiller import utils
+    while True:  # watch将导致数据库不同步 很烦
+        t = threading.Thread(target=utils.watchCourses)
+        t.start()
+        time.sleep(random.random() + watch_interval)
 
 
 class SCUKillerConfig(AppConfig):
