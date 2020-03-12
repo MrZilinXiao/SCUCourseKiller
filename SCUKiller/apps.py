4afe21d9669aa37d8c4e7b983ca26a7e08da4d7b
MrZilinXiao <me@mrxiao.net>
2020-03-12 15:58:15 +0800

    change sh to bash

--- SCUKiller/apps.py
+++ SCUKiller/apps.py
@@ -1,18 +1,4 @@
-import threading
-import time
-import random
 from django.apps import AppConfig
-from SCUKiller.config import watch_interval
-
-
-def watchLoop():
-    pass
-    from SCUKiller import utils
-    lock = threading.Lock()
-    while True:  # watch将导致数据库不同步 很烦
-        t = threading.Thread(target=utils.watchCourses, args=(None, lock))
-        t.start()
-        time.sleep(random.random() + watch_interval)
 
 
 class SCUKillerConfig(AppConfig):
@@ -20,7 +6,7 @@ class SCUKillerConfig(AppConfig):
 
     def ready(self):
         from .models import courses
-        courses.objects.all().update(inq=0)  # 启动时清空了队列
+        courses.objects.filter(isSuccess=0).update(inq=False)  # 启动时清空了队列
         # 考虑使用celery
         # watch_Loop = threading.Thread(target=watchLoop)
         # watch_Loop.setDaemon(True)
